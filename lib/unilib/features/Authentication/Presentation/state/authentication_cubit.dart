a8033650c32909d6eb0a91a5bb611c68490bb96a
Ad450 <adjeimanue@gmail.com>
2021-12-12 12:52:59 +0000

    authentication cubit completed. will be refactored later

--- lib/unilib/features/Authentication/Presentation/state/authentication_cubit.dart
+++ lib/unilib/features/Authentication/Presentation/state/authentication_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:library_project/unilib/core/app_typedefs.dart';
 import 'package:library_project/unilib/features/Authentication/Domain/UseCases/getVerificationCode.dart';
 import 'package:library_project/unilib/features/Authentication/Domain/UseCases/login.dart';
 import 'package:library_project/unilib/features/Authentication/Domain/UseCases/logout.dart';
@@ -20,4 +21,73 @@ class AuthenticationCubit extends Cubit<AuthenticationState> {
   AuthenticationCubit(this._getVerificationCode, this._login, this._logout,
       this._updateUser, this._verifyCode)
       : super(AuthenticationState.initial(error: "", user: null));
+
+  Future<void> login(String email, String password) async {
+    emit(AuthenticationState.loading(error: state.error, user: state.user));
+
+    final _result =
+        await _login.call(LoginParam(email: email, password: password));
+    _result.fold(
+        (l) =>
+            emit(AuthenticationState.error(error: l.message, user: state.user)),
+        (r) => emit(AuthenticationState.loaded(error: state.error, user: r)));
+  }
+
+  Future<void> getVerificationCode(
+      {required String email, required String password}) async {
+    emit(AuthenticationState.loading(error: state.error, user: state.user));
+
+    final _result = await _getVerificationCode
+        .call(GetVerificationCodeParam(email: email, password: password));
+    _result.fold(
+        (l) =>
+            emit(AuthenticationState.error(user: state.user, error: l.message)),
+        (r) => emit(
+            AuthenticationState.loaded(error: state.error, user: state.user)));
+  }
+
+  Future<void> updateUser(
+      {required String email,
+      required String oldPassword,
+      required String newPassword,
+      required String name,
+      required String id}) async {
+    emit(AuthenticationState.loading(user: state.user, error: state.error));
+
+    final _result = await _updateUser.call(UpdateUserParam(
+        email: email,
+        oldPassword: oldPassword,
+        name: name,
+        newPassword: newPassword,
+        id: id));
+    _result.fold(
+        (l) =>
+            emit(AuthenticationState.error(error: l.message, user: state.user)),
+        (r) => emit(AuthenticationState.loaded(error: state.error, user: r)));
+  }
+
+  Future<void> verifyCode({required String email, required String code}) async {
+    emit(AuthenticationState.loading(error: state.error, user: state.user));
+
+    final _result =
+        await _verifyCode.call(VerifyCodeParam(code: code, email: email));
+    _result.fold(
+        (l) => emit(
+              AuthenticationState.error(error: l.message, user: state.user),
+            ),
+        (r) => emit(
+            AuthenticationState.loaded(user: state.user, error: state.error)));
+  }
+
+  Future<void> logout() async {
+    emit(AuthenticationState.loading(error: state.error, user: state.user));
+
+    final _result = await _logout.call(NoParam());
+
+    _result.fold(
+        (l) =>
+            emit(AuthenticationState.error(error: l.message, user: state.user)),
+        (r) =>
+            AuthenticationState.loaded(error: state.error, user: state.user));
+  }
 }
