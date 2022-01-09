749c04c015922529c0097e59cbbe1d8002da776c
Ad450 <adjeimanue@gmail.com>
2022-01-09 20:29:45 +0000

    testing auth-login usecase

--- lib/unilib/core/utils/app_strings.dart
+++ lib/unilib/core/utils/app_strings.dart
@@ -9,16 +9,18 @@ abstract class AppStrings {
     ApiErrors.apiBadRequest: _apiBadRequest,
     ApiErrors.databaseException: _databaseException,
     ApiErrors.apiUnknown: _apiUknown,
-    ApiErrors.apiServerError: _apiServerError
+    ApiErrors.apiServerError: _apiServerError,
+    ApiErrors.apiGenericError: _apiGenericMessage
   };
 
   static String _apiUnauthenticated = "You need authentication to perform this action";
   static String _databaseException = "Database Access Failure";
-  static String _apiNoResponse = "Something went wrong";
+  static String _apiNoResponse = "No response from the server. Please check internet connection";
   static String _apiBadRequest = "There was a problem making the request to the server";
   static String _apiConnectionTimeout = "Connection time out";
   static String _apiUknown = "Something went wrong";
   static String _apiServerError = "something went wrong";
+  static String _apiGenericMessage = "something went wrong";
 }
 
 enum ApiErrors {
@@ -29,4 +31,5 @@ enum ApiErrors {
   databaseException,
   apiUnknown,
   apiServerError,
+  apiGenericError,
 }
