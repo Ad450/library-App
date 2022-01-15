8c324e1e2ccd20b116038f49d615c61faa4b6cab
Ad450 <adjeimanue@gmail.com>
2022-01-15 16:48:20 +0000

    login test passes

--- test/features/Authentication/domain/usecases/login_test.dart
+++ test/features/Authentication/domain/usecases/login_test.dart
@@ -1,25 +1,54 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:library_project/unilib/core/failures.dart';
 import 'package:library_project/unilib/features/Authentication/Domain/UseCases/login.dart';
 import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
 
 import '../../../../core/generate_mocks.mocks.dart';
 import 'package:library_project/unilib/features/Authentication/Data/Models/user_model/user_model.dart';
 
 void main() {
-  test("Given a registered name and password, then return a usermodel ", () async {
-    // Arrange
-    final mockUserRepository = MockUserRepository();
-    final login = Login(mockUserRepository);
-    final loginParam = LoginParam(email: 'adjeimanue@gmail.com', password: 'Manu450666');
-    final user = UserModel(email: 'adjeimanue@gmail.com', name: 'Emmanuel', password: 'Manu450666');
-    when(mockUserRepository.login(email: 'adjeimanue@gmail.com', password: 'Manu450666')).thenAnswer((_) async => user);
+  late MockUserRepository mockUserRepository;
+  late Login login;
+  setUp(() {
+    mockUserRepository = MockUserRepository();
+    login = Login(mockUserRepository);
+  });
+  group("Login usecase ", () {
+    test("should call only login from user repository", () {
+      when(mockUserRepository.login(email: "adjei@gmail.com", password: "manu"))
+          .thenAnswer((_) async => UserModel(email: "adjei@gmail.com", name: "adjei", password: "manu"));
 
-    // Act
+      login(LoginParam(email: "adjei@gmail.com", password: "manu"));
 
-    final actualUser = await login.call(loginParam);
+      verify(mockUserRepository.login(email: "adjei@gmail.com", password: "manu"));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
 
-    // Assert
+    test("should return a user model when provided info", () async {
+      // arrange
+      when(mockUserRepository.login(email: "adjei@gmail.com", password: "manu"))
+          .thenAnswer((realInvocation) async => UserModel(email: "adjei@gmail.com", name: "adjei", password: "manu"));
 
-    expect(actualUser.foldRight<UserModel>(user, (r, previous) => r), user);
+      // act
+      final result = await login(LoginParam(email: "adjei@gmail.com", password: "manu"));
+
+      // assert
+      expect(result.isRight(), true);
+      result.fold((l) => null, (r) => expect(r, isA<UserModel>()));
+    });
+
+    test("should return an error message when login fails", () async {
+      // arrange
+      final message = "could not login";
+      when(mockUserRepository.login(email: "adjei@gmail.com", password: "manu")).thenThrow(NetworkFailure(message));
+
+      // act
+      final result = await login(LoginParam(email: "adjei@gmail.com", password: "manu"));
+
+      //assert
+      expect(result.isLeft(), true);
+      result.fold((l) => expect(l.message, message), (r) => null);
+    });
   });
 }
