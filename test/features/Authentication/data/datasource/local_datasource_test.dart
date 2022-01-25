053a27e83c86b23d80bee1fd41aaba3969734b1f
Ad450 <adjeimanue@gmail.com>
2022-01-25 22:18:50 +0000

    local datasourceImpl test structure underway

---
+++ test/features/Authentication/data/datasource/local_datasource_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {});
+
+  group("local data source Impl", () {
+    group("cache user", () {});
+    group("get cached user", () {});
+    group("delete cached user", () {});
+  });
+}
